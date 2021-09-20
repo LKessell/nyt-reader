90a5c1142b1a1c5d6bfbd4004fc9ebeeacad260d
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 17:59:11 -0400

    Add function to get string of current date

---
+++ src/utilities.js
@@ -0,0 +1,9 @@
+export const getDateString = () => {
+  const date = new Date();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  
+  return `${month} ${day}, ${year}`;
+}

09a76ff4da24a80cd582a0e5c5178c69c48656f7
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 17:59:28 -0400

    Add date to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import homeIcon from '../../assets/home-icon.png';
 import './Header.css';
+import { getDateString } from '../../utilities';
 
 export const Header = () => {
   return (
     <header className='app-header'>
       <div></div>
+      <div>
       <h1>NY Times Reader</h1>
+      <p>{getDateString()}</p>
+      </div>
       <img src={homeIcon} alt='home icon' className='home-icon' />
     </header>
   )

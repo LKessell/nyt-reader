742bce1f4ddc41639e44bdab3f3356b0874d3e5f
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 15:52:54 -0400

    Render Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { Header } from '../Header/Header';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>NY Times Reader</p>
     </div>
   );

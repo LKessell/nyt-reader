f6d4be0f0f0f0a770cbb0866a886a60667199663
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 16:07:56 -0400

    Add class names, import CSS file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import homeIcon from '../../assets/home-icon.png';
+import './Header.css';
 
 export const Header = () => {
   return (
-    <header>
+    <header className='app-header'>
+      <div></div>
       <h1>NY Times Reader</h1>
       <img src={homeIcon} alt='home icon' className='home-icon' />
     </header>

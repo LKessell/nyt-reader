0ef12f8894a3ef6f978a7a9a20b21d5c8d3daea5
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 17:04:00 -0400

    Set article data in state when page is visited

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { fetchArticles } from '../../apiCalls';
 import { Header } from '../Header/Header';
 import { ArticleList } from '../ArticleList/ArticleList';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles()
+    .then(data => setArticles(data.results))
+  }, [])
+
   return (
     <div className="App">
       <Header />

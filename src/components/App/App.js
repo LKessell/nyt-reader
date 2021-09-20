8626dbc1ebfe5bf650aecc9c385ebfe3190846f8
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 16:38:50 -0400

    Render article list component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Header } from '../Header/Header';
+import { ArticleList } from '../ArticleList/ArticleList';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>NY Times Reader</p>
+      <main>
+        <Route exact path='/'>
+          <ArticleList />
+        </Route>
+      </main>
     </div>
   );
 }

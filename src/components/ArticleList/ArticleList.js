36f3a0bc0d80141f01302a2d9e9fd5cc8d9a758a
Lauren Kessell <lkessell1@gmail.com>
2021-09-20 17:28:49 -0400

    Render cards for each article in fetched data

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,9 +1,17 @@
+import { ArticleCard } from '../ArticleCard/ArticleCard';
 import './ArticleList.css';
 
-export const ArticleList = () => {
+export const ArticleList = ({ articles }) => {
+  const articleCards = articles.map(article => {
+    return (
+      <ArticleCard data={article} />
+    )
+  });
+  
   return (
     <section className='article-list'>
-      <h2>Top Stories</h2>
+      <h2 className='section-heading'>Top Stories</h2>
+      {articleCards}
     </section>
   )
 }

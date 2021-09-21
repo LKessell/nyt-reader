5d8321c8257d4788a246b508169d2b2692cb550c
Lauren Kessell <lkessell1@gmail.com>
2021-09-21 13:30:33 -0400

    Update tests

--- cypress/integration/detail_spec.js
+++ cypress/integration/detail_spec.js
@@ -1,6 +1,7 @@
 describe('Detail page user flows', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
+    cy.wait(1000);
   });
 
   it('Should be able to click an article to view its details', () => {
@@ -16,5 +17,21 @@ describe('Detail page user flows', () => {
           })
       })
     cy.url().should('include', '/articles/0');
+  });
+
+  it('Should show a title, date, image, abstract, and link to the full text on the details page', () => {
+    cy.get('article').first().click();
+    cy.get('p').should('have.length', 4);
+    cy.get('h2').should('be.visible');
+    cy.get('.detail-img').should('be.visible');
+    cy.get('a').contains('Click to continue reading...').should('be.visible');
+  });
+
+  it('Can click the home button to return to the homepage', () => {
+    cy.get('article').first().click();
+    cy.wait(500);
+    cy.get('h2').should('not.contain', 'Top Stories');
+    cy.get('.home-icon').click();
+    cy.get('h2').should('contain', 'Top Stories');
   });
 })

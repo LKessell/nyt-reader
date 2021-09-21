e1ed10a293e8534bad416cd1c0350349a6e78720
Lauren Kessell <lkessell1@gmail.com>
2021-09-21 17:32:03 -0400

    Add headings reference object

--- src/utilities.js
+++ src/utilities.js
@@ -12,3 +12,31 @@ export const getDateString = (dateArg) => {
 
   return `${month} ${day}, ${year}`;
 }
+
+export const headings = {
+  arts: 'Arts', 
+  automobiles: 'Automobiles', 
+  books: 'Books', 
+  business: 'Business', 
+  fashion: 'Fashion', 
+  food: 'Food', 
+  health: 'Health', 
+  insider: 'Insider', 
+  magazine: 'Magazine', 
+  movies: 'Movies', 
+  nyregion: 'New York', 
+  obituaries: 'Obituaries', 
+  opinion: 'Opinion', 
+  politics: 'Politics', 
+  realestate: 'Real Estate', 
+  science: 'Science', 
+  sports: 'Sports', 
+  sundayreview: 'Sunday Review', 
+  technology: 'Technology', 
+  theater: 'Theater', 
+  't-magazine': 'Magazine', 
+  travel: 'Travel', 
+  upshot: 'Upshot', 
+  us: 'U.S.', 
+  world: 'World',
+};

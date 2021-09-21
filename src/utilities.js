d384a7a5b1c8b1ae2a7a95f5ce1592e967dd0af9
Lauren Kessell <lkessell1@gmail.com>
2021-09-21 13:30:24 -0400

    Update date string utility

--- src/utilities.js
+++ src/utilities.js
@@ -1,8 +1,10 @@
-export const getDateString = (date) => {
-  if (!date) {
+export const getDateString = (dateArg) => {
+  let date;
+  if (!dateArg) {
     date = new Date();
+  } else {
+    date = new Date(dateArg);
   }
-  date = new Date(date);
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	const month = months[date.getMonth()];
   const day = date.getDate();

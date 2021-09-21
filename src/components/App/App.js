cce17579ab16b7faae0f02c754f3ea91f878d2aa
Lauren Kessell <lkessell1@gmail.com>
2021-09-21 17:36:58 -0400

    Render NavBar, move useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,27 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { fetchArticles } from '../../apiCalls';
+import { useState } from 'react';
 import { Header } from '../Header/Header';
 import { ArticleList } from '../ArticleList/ArticleList';
 import { ArticleDetail } from '../ArticleDetail/ArticleDetail';
-import { Route, Switch } from 'react-router-dom';
+import { NavBar } from '../NavBar/NavBar';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    fetchArticles()
-      .then(data => setArticles(data.results))
-  }, [])
-
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <main>
         <Switch>
           <Route exact path='/'>
-            <ArticleList articles={articles}/>
+            <Redirect to='/home' />
           </Route>
-          <Route exact path='/articles/:id'>
+          <Route exact path='/:type'>
+            <ArticleList articles={articles} setArticles={setArticles}/>
+          </Route>
+          <Route exact path='/:type/:id'>
             <ArticleDetail articles={articles}/>
           </Route>
         </Switch>

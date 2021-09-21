7afcb5efaac84873f638db51e2d0e7318693ba15
Lauren Kessell <lkessell1@gmail.com>
2021-09-21 12:17:20 -0400

    Update date function to take an argument

--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,12 @@
-export const getDateString = () => {
-  const date = new Date();
+export const getDateString = (date) => {
+  if (!date) {
+    date = new Date();
+  }
+  date = new Date(date);
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	const month = months[date.getMonth()];
   const day = date.getDate();
   const year = date.getFullYear();
-  
+
   return `${month} ${day}, ${year}`;
 }

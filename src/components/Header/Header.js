49fd0b6f9f8d42cad6faeabf8d6d7eda61ba6294
Lauren Kessell <lkessell1@gmail.com>
2021-09-22 16:18:15 -0400

    Add nav bar and state for shown/hidden

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import homeIcon from '../../assets/home-icon.png';
+import menuIcon from '../../assets/menu.png';
 import './Header.css';
+import { NavBar } from '../NavBar/NavBar';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getDateString } from '../../utilities';
 
 export const Header = () => {
+  const [navOpen, setNavOpen] = useState(window.innerWidth > 450);
+
   return (
-    <header className='app-header'>
-      <div></div>
-      <div>
-      <h1>NY Times Reader</h1>
-      <p>{getDateString()}</p>
-      </div>
-      <Link to='/'>
-        <img src={homeIcon} alt='home icon' className='home-icon' />
-      </Link>
-    </header>
+    <>
+      <header className='app-header'>
+        <button className='menu-toggle' onClick={() => setNavOpen(!navOpen)}>
+          <img src={menuIcon} alt='menu icon' className='menu-icon' />
+        </button>
+        <div>
+        <h1>NY Times Reader</h1>
+        <p>{getDateString()}</p>
+        </div>
+        <Link to='/'>
+          <img src={homeIcon} alt='home icon' className='home-icon' />
+        </Link>
+      </header>
+      <NavBar navOpen={navOpen}/>
+    </>
   )
 }

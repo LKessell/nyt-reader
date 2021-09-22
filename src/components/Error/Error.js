6e48776d2c3a5e5f5ed9b8b55b6d25b64478acab
Lauren Kessell <lkessell1@gmail.com>
2021-09-22 14:02:42 -0400

    Display error message based on response code

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,5 +1,22 @@
-export const Error = () => {
+import { Link } from 'react-router-dom';
+
+export const Error = ({ error }) => {
+  let errorMsg;
+  switch (error) {
+    case '429':
+      errorMsg = 'Too many requests made. Please wait a few moments before trying again.';
+      break;
+    case '404':
+      errorMsg = 'We\'re sorry, but the page you are looking for does not exist.';
+      break;
+    default:
+      errorMsg = 'We are currently experiencing server issues. Please try again later!';
+  }
+
   return (
-    <p>I am the error message!</p>
+    <>
+      <h3>{errorMsg}</h3>
+      <Link to='/home' className='home-link'>Back to Home</Link>
+    </>
   )
 }

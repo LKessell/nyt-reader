a07b4cf2ddd078a234c0aeffef7d031109317501
Lauren Kessell <lkessell1@gmail.com>
2021-09-22 12:44:03 -0400

    Add loading message

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,5 +1,5 @@
 import { ArticleCard } from '../ArticleCard/ArticleCard';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './ArticleList.css';
 import { fetchArticles } from '../../apiCalls';
@@ -7,16 +7,20 @@ import { headings } from '../../utilities';
 
 export const ArticleList = ({ articles, setArticles }) => {
   let params = useParams();
+  const [error, setError] = useState(null);
+
   const articleCards = articles.reduce((results, article, index) => {
     if (article.multimedia) {
       results.push(<ArticleCard key={index} id={index} data={article} type={params.type} />)
     }
     return results;
   }, []);
+  const loading = !articles.length && !error && <h3>Loading your articles...</h3>;
   
   useEffect(() => {
     fetchArticles(params.type)
       .then(data => setArticles(data.results))
+      .catch(err => setError(err.message))
   }, [params.type, setArticles]);
   
   let sectionHeading = headings[params.type] ? headings[params.type] : 'Top Stories';
@@ -24,6 +28,7 @@ export const ArticleList = ({ articles, setArticles }) => {
   return (
     <section className='article-list'>
       <h2 className='section-heading'>{sectionHeading}</h2>
+      {loading}
       {articleCards}
     </section>
   )

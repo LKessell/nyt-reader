a32c05a2f3ab1ec10bb805687fe111ae24254990
Lauren Kessell <lkessell1@gmail.com>
2021-09-22 14:00:28 -0400

    Move response to error handling function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,5 +2,4 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchArticles = (type) => {
   return fetch(`https://api.nytimes.com/svc/topstories/v2/${type}.json?api-key=${API_KEY}`)
-    .then(response => response.json())
 }
